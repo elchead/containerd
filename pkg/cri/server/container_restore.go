652281bd72a1866702eaa42dd4068db3d6032496
Adrian Stobbe <adrian.stobbe@sap.com>
2022-04-04 18:05:40 +0200

    f

--- pkg/cri/server/container_restore.go
+++ pkg/cri/server/container_restore.go
@@ -3,10 +3,8 @@ package server
 import (
 	"fmt"
 	"github.com/containerd/containerd"
-	"github.com/containerd/containerd/pkg/cri/util"
 	"golang.org/x/net/context"
 	runtime "k8s.io/cri-api/pkg/apis/runtime/v1alpha2"
-	"path/filepath"
 )
 
 // RestoreContainer restores a container from a previously created image.
@@ -17,10 +15,10 @@ func (c *criService) RestoreContainer(ctx context.Context, r *runtime.RestoreCon
 	// fmt.Println("Finished waiting restore")
 	checkPath := r.GetOptions().GetCheckpointPath()
 	zipPath := filepath.Join(filepath.Dir(checkPath), "check.zip")
-	fmt.Println("Restore here", save, zipPath)
+	fmt.Println("Restore here", checkPath, zipPath)
 	err := util.Unzip(zipPath, checkPath)
 	if err != nil {
-		return nil, err
+		return &runtime.RestoreContainerResponse{}
 	}
 	if err := c.startContainer(ctx, r.GetContainerId(), containerd.WithRestoreImagePath(r.GetOptions().GetCheckpointPath())); err != nil {
 		return nil, fmt.Errorf("failed to restore container: %v", err)

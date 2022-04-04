fced70680599195f29d8b0656e075ff735bf9b3e
Adrian Stobbe <adrian.stobbe@sap.com>
2022-04-04 11:41:49 +0200

    restore sleep

--- pkg/cri/server/container_restore.go
+++ pkg/cri/server/container_restore.go
@@ -5,11 +5,13 @@ import (
 	"github.com/containerd/containerd"
 	"golang.org/x/net/context"
 	runtime "k8s.io/cri-api/pkg/apis/runtime/v1alpha2"
+	"time"
 )
 
 // RestoreContainer restores a container from a previously created image.
 // It's essentially the same as starting a container with the additon of loading a checkpoint.
 func (c *criService) RestoreContainer(ctx context.Context, r *runtime.RestoreContainerRequest) (retRes *runtime.RestoreContainerResponse, retErr error) {
+	time.Sleep(20 * time.Second)
 	if err := c.startContainer(ctx, r.GetContainerId(), containerd.WithRestoreImagePath(r.GetOptions().GetCheckpointPath())); err != nil {
 		return nil, fmt.Errorf("failed to restore container: %v", err)
 	}
